bcee9d89e9aee27a5c6eda1770fb64af9f70e169
Rene Milk <koshi@springlobby.info>
2009-11-09 22:48:48 +0100

    fixing lots of syntax and flow errs

--- match.py
+++ match.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8 -*-
 
 from db_entities import *
-from datetime import datetime
+from datetime import datetime,timedelta
 from ranking import *
 
 def getSectionContect( string, name ):
 	b = string.find('BEGIN'+name)
 	e = string.find('END'+name)
-	return string[nb:e]
+	return string[b+len('BEGIN'+name)+1:e-1]
 
 def keyValPair(line):
 	key = line.split('=')[0]
 	val = line.split('=')[1]
-	return (val , key)
+	return (key , val)
 
-def parseSec( sec )
+def parseSec( sec ):
 	dic = dict()
-	for line in sec:
+	lines=sec.split('\n')
+	for line in lines:
+		if len(line) < 2:
+			continue
 		(key,val) = keyValPair(line)
 		dic[key] = val
 	return dic
 
-class MatchToDbWrapper(Object):
+class MatchToDbWrapper():
 
-	def __init__( self, stdout, battlefounder, ladder_id )
+	def __init__( self, stdout, battlefounder, ladder_id ):
 		self.springoutput 	= stdout
 		self.battlefounder 	= battlefounder
 		self.ladder_id		= ladder_id
@@ -31,44 +34,52 @@ class MatchToDbWrapper(Object):
 		self.game_over		= -1
 
 	def CommitMatch(self,db):
+		self.ParseSpringOutput()
+		ladder = db.GetLadder(self.ladder_id )
 		session = db.sessionmaker()
 		match = Match()
 		match.date 	= datetime.now() 
 		match.modname  = ''
 		match.mapname = ''
 		match.replay = ''
-		match.duration = 1
+		match.duration = timedelta(days=666)
+		match.ladder_id = ladder.id
 		session.add( match )
 		session.commit()
-		session.refresh()
-		for key,val in dict(self.options.items() + self.restr.items()):
+		#session.refresh()
+		for key,val in dict(self.options.items() + self.restr.items()).iteritems():
 			s = MatchSetting()
 			s.key = key
-			s.val = value
+			s.val = val
 			s.match_id = match.id
 			session.add( s )
 			session.commit()
 		self.CommitPlayerResults(session,match)
-		GlobalRankingAlgoSelector.GetInstance( match.ranking_algo_id ).Update( match.ladder_id, self, db )
+		session.close()
+		GlobalRankingAlgoSelector.GetInstance( 'simple' ).Update( ladder.id, self, db )
 
 	def CommitPlayerResults(self,session,match):
-		for name,result in self.players:
+		for name,result in self.players.iteritems():
 			p = session.query( Player ).filter( Player.nick == name ).first()
+			if not p:
+				p = Player( name )
+				session.add( p )
+				session.commit()
 			result.player_id = p.id
 			result.match_id = match.id
-			session.add( r )
+			session.add( result )
 			session.commit()
-		return results
+		#return results
 
 	def ParseSpringOutput(self):
 		setup_section 	= getSectionContect( self.springoutput, 'SETUP' )
-		
 		self.teams		= parseSec( getSectionContect( setup_section, 'TEAMS' 		) )
 		self.allies		= parseSec( getSectionContect( setup_section, 'ALLYTEAMS' 	) )
 		self.options 	= parseSec( getSectionContect( setup_section, 'OPTIONS' 	) )
 		self.restr		= parseSec( getSectionContect( setup_section, 'RESTRICTIONS') )
 
 		game_section 	= getSectionContect( self.springoutput, 'GAME' )
+		print 'game_section\n',game_section
 		num_players = len(self.teams)
 		self.players = dict()
 		
@@ -77,29 +88,29 @@ class MatchToDbWrapper(Object):
 			r.team = team
 			self.players[ name ] = r
 
-		for line in game_section:
+		for line in game_section.split('\n'):
 			tokens = line.split()
 			assert len(tokens) > 0
-			if token[0] == 'CONNECTED':
+			if tokens[0] == 'CONNECTED':
 				assert len(tokens) > 1
 				self.players[tokens[1]].connected = True
-			elif token[0] == 'DESYNC':
+			elif tokens[0] == 'DESYNC':
 				assert len(tokens) > 2
 				self.players[tokens[3]].desync = True
-			elif token[0] == 'LEAVE':
+			elif tokens[0] == 'LEAVE':
 				assert len(tokens) > 2
 				self.players[tokens[2]].quit = tokens[1]
-			elif token[0] == 'TEAMDIED':
+			elif tokens[0] == 'TEAMDIED':
 				assert len(tokens) > 2
 				for name,team_id in self.teams.iteritems():
 					if team_id == tokens[2]:
 						self.players[name].died = tokens[1]
-			elif token[0] == 'DISCONNECT':
+			elif tokens[0] == 'DISCONNECT':
 				assert len(tokens) > 2
 				self.players[tokens[2]].disconnect = tokens[1]
-			elif token[0] == 'GAMESTART':
+			elif tokens[0] == 'GAMESTART':
 				self.game_started = True
-			elif token[0] == 'GAMEOVER':
+			elif tokens[0] == 'GAMEOVER':
 				if not self.game_started:
 					print 'big bad error'
 				else:
