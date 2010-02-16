a349299960f95ac4db40b2a5ed2153761335c03c
Rene Milk <koshi@springlobby.info>
2010-02-16 21:37:16 +0100

    always wrote spring output to file

--- match.py
+++ match.py
@@ -148,6 +148,10 @@ class AutomaticMatchToDbWrapper(MatchToDbWrapper):
 		self.ladder_id		= ladder_id
 		self.game_started	= False
 		self.game_over		= -1
+		f  = file( 'last.output', 'w' )
+		f.write( stdout )
+		f.flush()
+		f.close()
 
 	def CheckvalidPlayerSetup( self, db ):
 		laddername = db.GetLadderName( self.ladder_id )
