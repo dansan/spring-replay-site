cf23d6057af32257a1f0dcf7baf9cd9b62cddfcb
dansan <daniel@admin-box.com>
2012-04-19 00:07:52 +0200

    separate secrets into extra file 2

---
+++ srs/mydb_.py
@@ -0,0 +1,11 @@
+# edit (see settings.DATABASES) and rename me to mydb.py
+
+my_con = {
+  'ENGINE'  : '',
+  'NAME'    : '',
+  'USER'    : '',
+  'PASSWORD': '',
+  'HOST'    : '',
+  'PORT'    : '',
+}
+
