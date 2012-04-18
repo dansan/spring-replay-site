17f4f549f0cf97b02b09a715460cdbac515c4594
dansan <daniel@admin-box.com>
2012-04-18 23:58:39 +0200

    cleanup

--- srs/models.py
+++ srs/models.py
@@ -9,7 +9,7 @@
 from django.db import models
 
 class Tag(models.Model):
-    name            = models.CharField(max_length=256, unique=True)
+    name            = models.CharField(max_length=128, unique=True)
 
     def __unicode__(self):
         return self.name
