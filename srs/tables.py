ceed7918e36f6b1455798ae8bd147f567e294730
dansan <daniel@admin-box.com>
2012-05-04 14:25:31 +0200

    copyright notices

--- srs/tables.py
+++ srs/tables.py
@@ -1,3 +1,11 @@
+# This file is part of the "spring relay site / srs" program. It is published
+# under the GPLv3.
+#
+# Copyright (C) 2012 Daniel Troeder (daniel #at# admin-box #dot# com)
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import django_tables2 as tables
 from django_tables2 import A
 
