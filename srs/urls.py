fc9969ec2a3e54d48d46b585544b10b0e010352b
dansan <daniel@admin-box.com>
2012-09-04 08:42:38 +0200

    fix last commit

--- srs/urls.py
+++ srs/urls.py
@@ -21,14 +21,14 @@ urlpatterns = patterns('',
     url(r'^mapmodlinks/(?P<gameID>\w+)/$', 'srs.views.mapmodlinks'),
     url(r'^feeds/latest_comments/$', LatestCommentFeed()),
     url(r'^feeds/latest/$', LatestUploadsFeed()),
-    url(r'^feeds/uploader/(?P<username>[\w\ .()\[\]-]+)/$', UploaderFeed()),
+    url(r'^feeds/uploader/(?P<username>[\w\ .:()\[\]-]+)/$', UploaderFeed()),
 
     url(r'^replays/$', 'srs.views.replays'),
     url(r'^replay/(?P<gameID>\w+)/$', 'srs.views.replay', name="replay_detail"),
     url(r'^edit_replay/(?P<gameID>\w+)/$', 'srs.views.edit_replay'),
 
     url(r'^tags/$', 'srs.views.tags'),
-    url(r'^tag/(?P<reqtag>[\w\ .-\[\]]+)/$', 'srs.views.tag', name="tag_detail"),
+    url(r'^tag/(?P<reqtag>[\w\ .:()\[\]-]+)/$', 'srs.views.tag', name="tag_detail"),
 
     url(r'^maps/$', 'srs.views.maps'),
     url(r'^map/(?P<mapname>[\w\ .-]+)/$', 'srs.views.rmap', name="map_detail"),
@@ -37,10 +37,10 @@ urlpatterns = patterns('',
     url(r'^player/(?P<accountid>[\d-]+)/$', 'srs.views.player', name="player_detail"),
 
     url(r'^games/$', 'srs.views.games'),
-    url(r'^game/(?P<gametype>[\w\ .:()-\[\]]+)/$', 'srs.views.game', name="game_detail"),
+    url(r'^game/(?P<gametype>[\w\ .:()\[\]-]]+)/$', 'srs.views.game', name="game_detail"),
 
     url(r'^users/$', 'srs.views.users'),
-    url(r'^user/(?P<username>[\w\ .()\[\]-]+)/$', 'srs.views.see_user', name="user_detail"),
+    url(r'^user/(?P<username>[\w\ .:()\[\]-]+)/$', 'srs.views.see_user', name="user_detail"),
 
     url(r'^match_date/(?P<shortdate>[\d-]+)/$', 'srs.views.match_date'),
     url(r'^upload_date/(?P<shortdate>[\d-]+)/$', 'srs.views.upload_date'),
