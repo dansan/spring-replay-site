a06e4e60c56f845f5b660c27714f1deed3091a18
dansan <daniel@admin-box.com>
2015-06-27 17:59:10 +0200

    forgot in last commit

---
+++ srs/mail.py
@@ -0,0 +1,27 @@
+# This file is part of the "spring relay site / srs" program. It is published
+# under the GPLv3.
+#
+# Copyright (C) 2015 Daniel Troeder (daniel #at# admin-box #dot# com)
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+logger = logging.getLogger(__package__)
+
+def send_mail(recipient_list, subject, mail_text, headers=None, cc=None, bcc=None, sender=settings.DEFAULT_FROM_EMAIL):
+    emailmsg = EmailMessage(subject=subject,
+                            body=mail_text,
+                            from_email=sender,
+                            to=recipient_list,
+                            headers=headers,
+                            cc=cc,
+                            bcc=bcc)
+
+    emailmsg.send()
+
+    logger.info("sent email from '%s' about '%s' to '%s' and bcc '%s'", sender, subject, recipient_list, bcc)
