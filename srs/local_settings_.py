88546afdd1b5f4f7e08a416d9b61e4211e55985f
Daniel Troeder <troeder@univention.de>
2018-11-16 17:30:32 +0100

    update IP

--- srs/local_settings_.py
+++ srs/local_settings_.py
@@ -46,7 +46,7 @@ SLDB_ACCOUNT = ""
 SLDB_PASSWORD = ""
 SLDB_SKILL_ORDER = [("1", 0,), ("F", 1), ("T", 2), ("G", 3), ("L", 4)]
 SLDB_TIMEOUT = 3
-SLDB_ALLOWED_IPS = ["78.46.100.156"]
+SLDB_ALLOWED_IPS = ["78.46.100.157"]
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "replays.springrts.com", "replays-test.springrts.com",
                  "replays.admin-box.com", "replays-test.admin-box.com", "78.46.100.156",
